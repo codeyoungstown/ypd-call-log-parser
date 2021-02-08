0deafbf4a3c4902ae72452604fe504a17418458b
Nicholas Serra <nickserra@gmail.com>
2021-02-07 21:08:37 -0500

    Working script

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # ypd-call-log-parser
 Parse image of YPD call logs via OCR to return text results
+
+# Installation
+
+- [Tesseract](https://github.com/tesseract-ocr/tesseract) is required on your path.
+- `pip install -r requirements.txt`
+
+# Usage
+This script expects a directory of images of call logs. Run `python logs.py --help` to see full options.
+
+```
+$ python logs.py /Users/nick/Desktop/images/ --exclude
+.
+201104046 13:46:44 11/20/20 | Fight/Weapon 1 123 S HAZELWOOD AVE YOU
+201104562 09:24:18 11/23/20 | Fight/Assault 2 123 S HAZELWOOD AVE YOU
+201201952 14:54:38 12/11/20 | Property Report dL 123 S HAZELWOOD AVE YOU
+201204356 21:34:38 12/24/20 | Unknown Trouble 1 123 S HAZELWOOD AVE; MAPPED TO YOU
+201204438 11:44:43 12/25/20 | Fight/Assault 2 123 S HAZELWOOD AVE YOu
+210100778 17:06:11 01/04/21 | Property Report 1 123 S HAZELWOOD AVE YOU
+210101459 15:34:57 01/08/21 | Fight/Assault 2 123 S HAZELWOOD AVE YOU
+210101491 17:09:39 01/08/21 | Investigation 8 ALMACT &S HAZELWOOD AVE YOU
+210102654 18:41:42 01/14/21 | Suspicious 3 WALDEN CT & S HAZELWOOD AVE YOU
+210102786 12:00:39 01/15/21 e Unknown Trouble = 1 123 S HAZELWOOD AVE You
+210103080 23:57:58 01/16/21 | Investigation 8 123 S HAZELWOOD AVE YOU
+210103093 01:11:51 01/17/21 | Noise Complaint a 123 S HAZELWOOD AVE YOU
+210103096 01:33:06 01/17/21 | Investigation 8 123 S HAZELWOOD AVE YOU
+```
---
+++ logs.py
@@ -0,0 +1,102 @@
+import argparse
+import os
+import re
+import sys
+
+import pytesseract
+
+from PIL import Image
+
+
+EXCLUDE_KEYWORDS = [
+    'amr', 'lift', 'animal', 'alarm', 'structure', 'medical', 'persons',
+    'parking', 'dog', 'accident', 'assist', 'paper'
+]
+
+ID_RE = re.compile(r'^\d{9}')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('directory', type=str, help='Directory containing images to parse')
+    parser.add_argument('filter_nature', type=str, help='Comma separated list of natures to filter by', nargs='?', default=None)
+    parser.add_argument('--exclude', dest='exclude_lines', action='store_true',
+                        default=False, help='Exclude non emergency nature')
+    parser.add_argument('--lines', dest='output_lines', action='store_true',
+                        default=True, help='Output call lines only')
+    parser.add_argument('--ids', dest='output_ids', action='store_true',
+                        default=False, help='Output ids only')
+    args = parser.parse_args()
+
+    directory = args.directory
+    nature_filters = args.filter_nature
+    exclude_lines = args.exclude_lines
+
+    if nature_filters:
+        nature_filters = [f.strip() for f in nature_filters.split(',')]
+
+    try:
+        pages = []
+        for filename in os.listdir(directory):
+            if filename.endswith('.jpeg') or filename.endswith('.jpg'):
+
+                sys.stdout.write('.')
+                sys.stdout.flush()
+
+                filepath = os.path.join(directory, filename)
+
+                # Images to line text
+                text = pytesseract.image_to_string(Image.open(filepath), config='--psm 6')
+                pages.append(text.split('\n'))
+    except FileNotFoundError:
+        sys.stderr.write('Directory not found.\n')
+        sys.exit(1)
+
+    if not pages:
+        sys.stderr.write('No images found.\n')
+        sys.exit(1)
+
+    sys.stdout.write('\n')
+
+    # Sanitize lines
+    clean_lines = []
+    for page in pages:
+
+        for line in page:
+
+            # Skip any lines that don't start with 9 digit call ID
+            id_match = ID_RE.match(line)
+            if not id_match:
+                continue
+
+            # Skip any lines with excluded reasons
+            if exclude_lines and any(word.lower() in line.lower() for word in EXCLUDE_KEYWORDS):
+                continue
+
+            # If a filter is set, skip any that don't match
+            if nature_filters:
+                if not any(word.lower() in line.lower() for word in nature_filters):
+                    continue
+
+            clean_lines.append(
+                # Call ID, line
+                (id_match.group(), line)
+            )
+
+    if not clean_lines:
+        sys.stderr.write('No results\n')
+        sys.exit(1)
+
+    # Output comma separated list of IDs only
+    if args.output_ids:
+        output_ids = ', '.join([l[0] for l in clean_lines])
+        sys.stdout.write('{}\n'.format(output_ids))
+
+    # Output full lines
+    else:
+        for line in clean_lines:
+            sys.stdout.write('{}\n'.format(line[1]))
+
+
+if __name__ == "__main__":
+    main()
